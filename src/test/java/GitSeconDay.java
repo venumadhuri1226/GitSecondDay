197cc80faf7101fbca1d5596682f01f6d1741448
venumadhuri1226 <k.venumadhuri1227@gmail.com>
2019-12-12 10:10:20 +0530

    i am done load URL

--- src/test/java/GitSeconDay.java
+++ src/test/java/GitSeconDay.java
@@ -1,6 +1,8 @@
 
-public class GitSeconDay {
+public class GitSeconDay extends BaseClass{
 public static void main(String[] args) {
 	System.out.println("Hi");
+	launchBroser();
+	loadUrl("https://www.facebook.com/");
 }
 }
